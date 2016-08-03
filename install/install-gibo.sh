762d0e8dbb217905b0b86f799ddb8f4b6e3664d2
akira.takahashi <rike422@gmail.com>
2016-08-04 01:46:17 +0900

    Add install-gibo.sh
    https://github.com/simonwhitaker/gibo

---
+++ install/install-gibo.sh
@@ -0,0 +1,6 @@
+
+###################
+# intall gibo
+###################
+
+curl -L https://raw.github.com/simonwhitaker/gibo/master/gibo -so $DOTFILES/bin/gibo && chmod +x $DOTFILES/bin/gibo && gibo -u
