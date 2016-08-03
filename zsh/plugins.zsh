fb860dc8d1497f4ba8f01b6a1611fe387ec190f2
akira.takahashi <rike422@gmail.com>
2016-08-03 20:17:13 +0900

    Add zsh plugins
    * zsh-gomi(https://github.com/b4b4r07/zsh-gomi)
    * fzf(https://github.com/junegunn/fzf)

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -4,5 +4,9 @@ zplug "zsh-users/zsh-completions"
 # 絵文字
 zplug "mrowa44/emojify", as:command
 
+# インタラクティブフィルタ
+zplug "junegunn/fzf-bin", as:command, from:gh-r, rename-to:fzf
+zplug "junegunn/fzf", as:command, use:bin/fzf-tmux
+
 # ゴミ箱
-zplug "b4b4r07/zsh-gomi", as:command, use:bin
+zplug "b4b4r07/zsh-gomi", if:"which fzf"
