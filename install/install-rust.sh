65de76c743e33b120a9012ac94359305f25f1b79
akira.takahashi <rike422@gmail.com>
2016-08-05 02:22:07 +0900

    [Install] update rustup

--- install/install-rust.sh
+++ install/install-rust.sh
@@ -5,7 +5,10 @@
 
 DEST=$DOTFILES/pkg/rust
 RUSTUP_HOME=$DEST
+TMP_RUSTUP=$RUSTUP_HOME/rustup.sh
 
 rm -rf $DEST
-curl https://sh.rustup.rs -sSf | sh
-eval $RUSTUP_HOME
+mkdir $DEST
+curl https://sh.rustup.rs -sSf > $TMP_RUSTUP
+chmod +x $TMP_RUSTUP
+eval $TMP_RUSTUP -y  --no-modify-path
