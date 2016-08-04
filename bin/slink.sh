a5833b1a60333e0c02f57209619b9413beab3113
akira.takahashi <rike422@gmail.com>
2016-08-04 14:16:43 +0900

    Add peco config.json

--- bin/slink.sh
+++ bin/slink.sh
@@ -1,4 +1,4 @@
-dotfiles=".vimrc .vim .ideavimrc .gemrc .gitconfig .gitignore_global .zshrc .tmux.conf .hgrc .npmrc .tigrc .pryrc"
+dotfiles=".vimrc .vim .ideavimrc .gemrc .gitconfig .gitignore_global .zshrc .tmux.conf .hgrc .npmrc .tigrc .pryrc .peco"
 for dotfile in $dotfiles
 do
   ln -s "$HOME/dotfiles/$dotfile" $HOME
