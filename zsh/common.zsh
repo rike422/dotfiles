dfa28a131eb30f85eb9dcc38f9927dcb00bf6943
Akira Takahashi <rike422@gmail.com>
2018-03-28 11:41:51 +0900

    [zsh] Add aliases for docker-compose

--- zsh/common.zsh
+++ zsh/common.zsh
@@ -44,6 +44,24 @@ alias ber="bundle exec rails"
 alias berc="bundle exec rails c"
 alias beg="bundle exec guard"
 
+# Docker
+
+alias dc='docker-compose'
+alias dcb='docker-compose build'
+alias dce='docker-compose exec'
+alias dcps='docker-compose ps'
+alias dct='docker-compose top'
+alias dcrestart='docker-compose restart'
+alias dcrm='docker-compose rm'
+alias dcr='docker-compose run'
+alias dcstop='docker-compose stop'
+alias dcup='docker-compose up'
+alias dcdn='docker-compose down'
+alias dcl='docker-compose logs'
+alias dclf='docker-compose logs -f'
+alias dcpull='docker-compose pull'
+alias dcstart='docker-compose start'
+
 ### Commnads ###
 
 get_github_latest_release() {
