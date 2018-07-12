2cadb1d974ea7229108daaabb3ef81fe3467cc1c
Akira Takahashi <rike422@gmail.com>
2018-07-12 23:24:38 +0900

    [install] install-protobuf.sh

---
+++ install/install-protobuf-rules-gen.sh
@@ -0,0 +1,19 @@
+TMP_DIR=/tmp/protobuf-rules-gen
+TMP_FILE=/tmp/protobuf-rules-gen
+DEST=$DOTFILES/bin/protobuf-rules-gen
+
+if [ $(uname) = "Linux" ]; then
+	PLATFORM='linux-x86_64'
+	$DOTFILES/bin/github_latest_release.sh firebase protobuf-rules-gen $PLATFORM | xargs wget -O $TMP_FILE
+	unzip -d $TMP_FILE -d $TMP_DIR
+	mv $TMP_DIR/bin/protobuf-rules-gen $DEST
+else
+	PLATFORM='osx-x86_64'
+	$DOTFILES/bin/github_latest_release.sh firebase protobuf-rules-gen $PLATFORM | xargs wget -O $TMP_FILE
+	unzip $TMP_FILE -d $TMP_DIR
+	mv $TMP_DIR/bin/protobuf-rules-gen $DEST
+fi
+
+echo "Install $(which protobuf-rules-gen)"
+rm $TMP_FILE
+rm -rf $TMP_DIR
